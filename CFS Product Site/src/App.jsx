79ea66176d47769984409a13b418d65c8b78f180
BadalNarayan123 <badalgupta78550@gmail.com>
2024-12-19 16:23:19 +0530

    #commit

--- CFS Product Site/src/App.jsx
+++ CFS Product Site/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import {BrowserRouter , Router,Routes} from 'react-router-dom'
+import {BrowserRouter , Route,Routes} from 'react-router-dom';
+import Home from './Pages/Home';
+import Product from './Pages/Product';
+import Blog from './Pages/Blog';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+
 const App = () => {
   return (
    <>
+   <BrowserRouter>
     <Navbar/>
+    <Routes>
+      <Route path='/' element={<home/>}/>
+      <Route path='/' element={<Product/>}/>
+      <Route path='/' element={<Blog/>}/>
+      <Route path='/' element={<About/>}/>
+      <Route path='/' element={<Contact/>}/>
+    </Routes>
+   </BrowserRouter>
    </>
   )
 }
